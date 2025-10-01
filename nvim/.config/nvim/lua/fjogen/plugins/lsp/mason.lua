0e0a40f7eba12b0630c2de5583465e5325a7cd1f
Fjoggs <vegard@fjobeno.com>
2025-10-01 23:23:43 +0200

    Rewrite lspconfig to be much simpler + use basedpyright instead of pyright

--- nvim/.config/nvim/lua/fjogen/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/fjogen/plugins/lsp/mason.lua
@@ -1,6 +1,11 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
 	opts = {},
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "basedpyright", "ruff" },
+		})
+	end,
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
